2702e8d477e80f997178d13df87fb4a417bb6c4b
Shane Arcaro <smarcaro@gmail.com>
2022-02-03 12:12:16 -0500

    Main has definitions of language characteristics

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -9,15 +7,36 @@
  * https://web.njit.edu/~marvin/cs341/projects/prog1email.pdf
  */
 
+#define GAMMA_LENGTH 26
+#define DELTA_LENGTH 1
+#define PHI_LENGTH 1
+#define SIGMA_LENGTH GAMMA_LENGTH + DELTA_LENGTH + PHI_LENGTH
+
+// Set of lower-case Roman letters
+std::string GAMMA[GAMMA_LENGTH] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+// Set of a period
+std::string DELTA[DELTA_LENGTH] = {"."};
+
+// Set of an at symbol
+std::string PHI[PHI_LENGTH] = {"@"};
+
+// Set of all available letters
+std::string SIGMA[SIGMA_LENGTH];
+
 // Prints the opening messages required in project instructions
 void printOpening();
 
 // State machine that handles user input
 void dfa(std::string input);
 
+// Just needed a function to concat everything into SIGMA
+void concat();
+
 int main(int argc, char* argv[]) {
     // Print opening has to be first
     printOpening();
+    concat();
 
     // Ask user if they want to enter a string and continue on with the program; otherwise terminate
     std::string user_input;
@@ -43,7 +62,6 @@ int main(int argc, char* argv[]) {
             // File could not be read, must be string input
             dfa(input);
         }
-        
     }
     else if (user_input == "N" || user_input == "n") {
         std::cout << "No input string being read. Terminating." << std::endl;
@@ -66,4 +84,12 @@ void printOpening() {
 
 void dfa(std::string input) {
     std::cout << "From DFA: " << input << std::endl;
+}
+
+void concat() {
+    for (int i = 0; i < GAMMA_LENGTH; i++) {
+        SIGMA[i] = GAMMA[i];
+    }
+    SIGMA[GAMMA_LENGTH] = DELTA[0];
+    SIGMA[GAMMA_LENGTH + DELTA_LENGTH] = PHI[0];
 }
