b0b4f57adb8a2e12e01cc0a63f7fcf3e189e0201
Shane Arcaro <smarcaro@gmail.com>
2022-02-05 21:18:11 -0500

    Fixing Node formatting

--- project1/include/node.h
+++ project1/include/node.h
@@ -18,16 +18,29 @@ class Node {
         bool state;
 
     public: 
+        // Create a regular node with default accept state
         Node(int id): id(id), state(false) {};
+
+        // Create an accept state node
         Node(int id, bool acceptState): id(id), state(acceptState) {};
-        
-        // void addTransition(std::string accept, int id);
-        void addTransition(std::string accept, int id);
-        int next(std::string input);
-        std::vector<Transition> getTransitions();
+
+        // Get the current state of a node (if it's an accepting node)
         bool getState();
+
+        // Get the unique id of a node
         int getID();
-        void setID(int id);
+        
+        // Add a new transition between the current node and a new node
+        // The transition is only followed if the accept string matches the input string
+        void addTransition(std::string accept, int id);
+
+        //Get a list of all transitions a node has
+        std::vector<Transition> getTransitions();
+
+        // Given an input character determine which transition should be followed
+        // Return the node id of the transitioned to node
+        int next(std::string input);
+
 };
 
 #endif
--- project1/src/node.cpp
+++ project1/src/node.cpp
@@ -2,27 +2,49 @@
 
 #include "../include/node.h"
 
+// Get the current state of a node (if it's an accepting state)
+bool Node::getState() {
+    return state;
+}
+
+// Get the unique id of a node
+int Node::getID() {
+    return id;
+}
+
+// Add a new transition between the current node and a new node
+// The transition is only followed if the accept string matches the input string
 void Node::addTransition(std::string accept, int id) {
     Transition t(accept, this->getID(), id);
     this->transitions.push_back(t);
 }
 
-// Transition to the next node given input
+// Get a list of all transitions a node has
+std::vector<Transition> Node::getTransitions() {
+    return this->transitions;
+}
+
+// Given an input character determine which transition should be followed
+// Return the node id of the transitioned to node
 int Node::next(std::string input) {
-    for (int i = 0; i < transitions.size(); i++) {
+    // Iterate through every transition looking for an accept
+    // If a transition is never found follow the trash transition (the last transition)
+    for (int i = 0; i < transitions.size() - 1; i++) {
         Transition* p_temp = &transitions.at(i);
         // If input is a direct match
         if (input.compare(p_temp->getAccept()) == 0) {
             return p_temp->getTo();
         }
-        // G is designated for roman lowercase letters
+        // If the input belongs to the GAMMA set (lower case letters)
         else if (p_temp->getAccept().compare("G") == 0) {
             // If input is a letter
             if (isalpha(input[0])) {
                 return p_temp->getTo();
             }
         }
+        // If the input belongs to the GAMMA set except for x in G-x
         else if (p_temp->getAccept().substr(0, 2).compare("G-") == 0) {
+            // Find out which letter should be ignored
             std::string letter = p_temp->getAccept().substr(2);
             if (isalpha(input[0]) && input.compare(letter) != 0) {
                 return p_temp->getTo();
@@ -30,24 +52,7 @@ int Node::next(std::string input) {
         }
     }
 
+    // No transition was found: follow trash transition
     Transition* p_trash = &transitions.back();
     return p_trash->getTo();
-}
-
-std::vector<Transition> Node::getTransitions() {
-    return this->transitions;
-}
-
-// Return the state of the node
-bool Node::getState() {
-    return state;
-}
-
-// Get the ID of the node
-int Node::getID() {
-    return id;
-}
-
-void Node::setID(int id) {
-    this->id = id;
 }
