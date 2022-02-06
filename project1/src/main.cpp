641226be5f5bcf77d80d56dffe1164f23357703e
Shane Arcaro <smarcaro@gmail.com>
2022-02-06 09:01:17 -0500

    DFA L2 already checked L1 and L2 because of Kleene Star Closure principles, condensed everything into a single DFA

--- project1/src/main.cpp
+++ project1/src/main.cpp
@@ -24,25 +24,18 @@
 // S2 = ΔΓΓ*
 // S3 = {.org}
 
-// DFA L1: Responsible for analyzing strings against L1 = S1ΦS1S3
-DFA L1;
-
-// DFA L2: Responsible for analyzing strings against L2 = S1S2*ΦS1S2*S3
-DFA L2;
+// DFA : Responsible for analyzing strings against L = L1 U L2, where L1 = S1ΦS1S3 and L2 = S1S2*ΦS1S2*S3
+DFA dfa;
 
 // Print the required opening text
 void printOpening();
 
-// Create the nodes and set up the required transitions for the DFA L1 to work
-void createL1();
-
-// Create the nodes and set up the required transitions for the DFA L2 to work
-void createL2();
+// Create the nodes and set up the required transitions for the DFA to work
+void createDFA();
 
 // Analyze a given string on both DFAs L1 and L2
 // A string only needs to pass one of the DFAs to be accepted
 void start(std::string input);
-
 
 int main(int argc, char* argv[]) {
     // Concatenate the sets together
@@ -51,9 +44,8 @@ int main(int argc, char* argv[]) {
     // Print the opening, has to be the first printed statements
     printOpening();
 
-    // Set up both DFAs
-    createL1();
-    createL2();
+    // Set up the DFA
+    createDFA();
 
     // Ask user if they want to enter a string and continue on with the program; otherwise terminate
     std::string user_input;
@@ -81,9 +73,8 @@ int main(int argc, char* argv[]) {
                     getline(file, input);
                     start(input);
 
-                    // Need to reset each DFA's head node in order to be run again
-                    L1.reset();
-                    L2.reset();
+                    // Need to reset the DFA's head node in order to be run again
+                    dfa.reset();
                 }
                 // Close the file being read from
                 file.close();
@@ -92,9 +83,8 @@ int main(int argc, char* argv[]) {
                 // File could not be read, must be string input
                 start(input);
 
-                // Need to reset each DFA's head node in order to be run again
-                L1.reset();
-                L2.reset();
+                // Need to reset the DFA's head node in order to be run again
+                dfa.reset();
             }
         }
         // If user does not want to enter a string or file terminate the program
@@ -118,22 +108,11 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-// Analyze a given string on both DFAs L1 and L2
-// A string only needs to pass one of the DFAs to be accepted
+// Analyze a given string on the DFA
 void start(std::string input) {
-    // Test L1
-    std::cout << "Testing L1" << std::endl;
-    bool test1 = L1.next(input);
-    std::cout << std::endl;
+    bool test = dfa.next(input);
 
-    // Test L2
-    std::cout << "Testing L2" << std::endl;
-    bool test2 = L2.next(input);
-
-    // Print result
-    std::cout << std::endl;
-    std::cout << "L1" << ((test1) ? ": accepted" : ": rejected") << "\tL2" << ((test2) ? ": accepted" : ": rejected") << std::endl;
-    std::cout << "Final Result: " << input << ((test1 || test2) ? " is accepted" : " is rejected") << std::endl;
+    std::cout << "Final Result: " << input << ((test) ? " is accepted" : " is rejected") << std::endl;
     std::cout << std::endl;
 }
 
@@ -146,40 +125,12 @@ void printOpening() {
     std::cout << "Instructor: Marvin Nakayama, marvin@njit.edu" << std::endl;
 }
 
-// Create the nodes and set up the required transitions for the DFA L1 to work
-void createL1() {
+// Create the nodes and set up the required transitions for the DFA to work
+void createDFA() {
     // Create each node
-    Node q1(L1.getTicket()), q2(L1.getTicket()), q3(L1.getTicket());
-    Node q4(L1.getTicket()), q5(L1.getTicket()), q6(L1.getTicket());
-    Node q7(L1.getTicket()), q8(L1.getTicket(), true), trash_node(L1.getTicket());
-    
-    // Add transitions between the nodes
-    q1.addTransition("G", q2.getID());
-    q2.addTransition("G", q2.getID());
-    q2.addTransition("@", q3.getID());
-    q3.addTransition("G", q4.getID());
-    q4.addTransition("G", q4.getID());
-    q4.addTransition(".", q5.getID());
-    q5.addTransition("o", q6.getID());
-    q6.addTransition("r", q7.getID());
-    q7.addTransition("g", q8.getID());
-
-    // Each node needs a final transition added to be able to reach the trash node
-    // Trash node transition is only used if the DFA cannot continue properly
-    std::vector<Node> nodes = {q1, q2, q3, q4, q5, q6, q7, q8, trash_node};
-    for (int i = 0; i < nodes.size(); i++) {
-        nodes.at(i).addTransition("X", trash_node.getID());
-    }
-    // Put the nodes into the DFA
-    L1.setNodes(nodes);
-}
-
-// Create the nodes and set up the required transitions for the DFA L2 to work
-void createL2() {
-    // Create each node
-    Node q1(L2.getTicket()), q2(L2.getTicket()), q3(L2.getTicket());
-    Node q4(L2.getTicket()), q5(L2.getTicket()), q6(L2.getTicket());
-    Node q7(L2.getTicket()), q8(L2.getTicket(), true), trash_node(L2.getTicket());
+    Node q1(dfa.getTicket()), q2(dfa.getTicket()), q3(dfa.getTicket());
+    Node q4(dfa.getTicket()), q5(dfa.getTicket()), q6(dfa.getTicket());
+    Node q7(dfa.getTicket()), q8(dfa.getTicket(), true), q9(dfa.getTicket());
 
     // Add transitions between the nodes
     q1.addTransition("G",   q2.getID());
@@ -199,12 +150,12 @@ void createL2() {
     q7.addTransition(".",   q5.getID());
     q8.addTransition(".",   q5.getID());
 
-    // Each node needs a final transition added to be able to reach the trash node
-    // Trash node transition is only used if the DFA cannot continue properly
-    std::vector<Node> nodes = {q1, q2, q3, q4, q5, q6, q7, q8, trash_node};
+    // Each node needs a final transition added to be able to reach the trap node (q9)
+    // Trap node transition is only used if the DFA cannot continue properly
+    std::vector<Node> nodes = {q1, q2, q3, q4, q5, q6, q7, q8, q9};
     for (int i = 0; i < nodes.size(); i++) {
-        nodes.at(i).addTransition("X", trash_node.getID());
+        nodes.at(i).addTransition("X", q9.getID());
     }
     // Put the nodes into the DFA
-    L2.setNodes(nodes);
+    dfa.setNodes(nodes);
 }
